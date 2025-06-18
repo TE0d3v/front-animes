a5d1b1dc1e6a42a8f8f42c10ab12c57e1a824968
Pedro <pedrohenriqueteofilo2707@gmail.com>
2025-06-17 21:21:30 -0300

    finalizando aula

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -1,13 +1,39 @@
+import instance from "@/instance/api"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import Header from "../Header"
+
 export default function PageWrapper({ children }) {
+
+    const router = useRouter()
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if (!token) {
+            router.push('/')
+        }
+
+        async function heartBeat() {
+            try {
+                await instance.get('/heartbeat')
+            } catch (error) {
+                localStorage.removeItem('token')
+                router.push('/')
+            }
+        }
+
+        heartBeat()
+        // Simula um delay para mostrar o loading
+        setLoading(false)
+    }, [])
+
     return (
-        <div className="w-full h-full flex flex-col">
-            <header className="w-full h-[50px] bg-red-300"></header>
-
-            <div className="w-full h-[85%]">
-                {children}
+        <div className="w-full min-h-screen flex flex-col">
+            <Header />
+            <div className="w-full h-full pt-[65px]">
+            {children}
             </div>
-
-            <footer className="w-full h-[50px] bg-blue-300"></footer>
         </div>
     )
 }
--- src/pages/animes/index.jsx
+++ src/pages/animes/index.jsx
@@ -4,27 +4,7 @@ import { useRouter } from "next/router"
 import { Children, useEffect, useState } from "react"
 
 export default function Animes(){
-    const router = useRouter()
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-
-        if(!token){
-            router.push('/')
-        }
-
-        async function heartBeat() {
-            try{
-                await instance.get('/heartbeat')
-            }catch(error){
-                router.push('/')
-            }
-        }
-
-        heartBeat()
-        // Simula um delay para mostrar o loading
-        setLoading(false)
-    }, [])
+    
     
     return(
         <PageWrapper>
