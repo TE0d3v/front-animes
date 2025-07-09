5b3d2cd4b1ecd554fdcbb0e7a6ff88acf10e551b
Pedro <pedrohenriqueteofilo2707@gmail.com>
2025-07-08 21:49:11 -0300

    implementaçaõ do microsoft clarity no front

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,27 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <ToastContainer 
+      <ToastContainer
         position="top-right"
         autoClose={3000}
         theme="dark"
+      />
+      <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "sby9978gj8");`
+        }}
+
       />
       <Component {...pageProps} />
     </>
